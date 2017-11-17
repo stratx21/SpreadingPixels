9f3e799592226ae3f2977f59367b7ef06f6824da
Josh <stratx21@gmail.com>
2017-11-16 23:22:48 -0600

    The general program is working. The Yellow seems much more dominant.

---
+++ src/pixelsfighting/Pixel.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pixelsfighting;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Josh
+ */
+public class Pixel{
+    /**
+     * The identifier for the team this Pixel instance is on.
+    */
+    private boolean leftSide=true;
+    
+    /**
+     * The coordinate values of this Pixel instance. 
+     */
+    private int x=0,y=0;
+    
+    /**
+     * The width and height (in pixels) of this Pixel instance (this is the
+     *      same for all Pixel objects).
+     */
+    private static int width=1;
+    
+    private static final Color left  = new Color(26, 102,46);
+    private static final Color right = new Color(212,175,55);
+    
+    public Pixel(int xnew,int ynew,boolean left){
+        x=xnew;
+        y=ynew;
+        leftSide=left;
+    }
+    
+    public void draw(Graphics g){
+        if(leftSide)
+            g.setColor(left);
+        else
+            g.setColor(right);
+        
+        g.fillRect(x, y, width, width);
+    }
+    
+    /**
+     * This function returns a boolean value concerning if this Pixel
+     *      instance is on the left side team. 
+     * 
+     * @return if this Pixel is on the left side team.
+     */
+    public boolean getSide(){
+        return leftSide;
+    }
+    
+    public void setSide(boolean leftNew){
+        leftSide=leftNew;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public void setX(int n){
+        x=n;
+    }
+    
+    public void setY(int n){
+        y=n;
+    }
+    
+    public static void setWidth(int w){
+        width=w;
+    }
+}
---
+++ src/pixelsfighting/PixelsFighting.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pixelsfighting;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Josh
+ */
+public class PixelsFighting extends JPanel{
+    private final int FRAME_SIDE_LENGTH=500;
+    private final int PIXEL_SIDE=2;
+    private int NUMBER_OF_PIXELS;
+    
+    private JFrame frame = new JFrame();
+    
+    private Pixel[][] pixels=new Pixel[FRAME_SIDE_LENGTH][FRAME_SIDE_LENGTH];
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new PixelsFighting();
+    }
+    
+    public PixelsFighting(){
+        
+        NUMBER_OF_PIXELS=FRAME_SIDE_LENGTH/PIXEL_SIDE;
+        Pixel.setWidth(PIXEL_SIDE);
+        
+        //Output/display setup::
+        this.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setSize(500,500);
+        
+        
+        
+        
+        
+        //Pixel objects setup::
+        for(int y=0;y<NUMBER_OF_PIXELS;y++){
+            boolean toBeLeft=y<((NUMBER_OF_PIXELS)/2);
+            for(int x=0;x<NUMBER_OF_PIXELS;x++){
+                pixels[y][x]=new Pixel(x*PIXEL_SIDE,y*PIXEL_SIDE,toBeLeft);
+            }
+        }
+        
+        
+        frame.add(this);
+        frame.setVisible(true);  
+        this.setVisible(true);
+        this.repaint();
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        boolean thisPixelColor;
+        for(int x=0;x<NUMBER_OF_PIXELS;x++){
+            for(int y=0;y<NUMBER_OF_PIXELS;y++){//Inverted the loop
+                
+                pixels[y][x].draw(g);
+                
+                thisPixelColor=pixels[y][x].getSide();
+                
+                switch((int)(Math.random()*4)){
+                    case 0:
+                        if(y>0)
+                            pixels[y-1][x].setSide(thisPixelColor);
+                        break;
+                    case 1:
+                        if(x>0)
+                            pixels[y][x-1].setSide(thisPixelColor);
+                        break;
+                    case 2:
+                        if(x<NUMBER_OF_PIXELS-1)
+                            pixels[y][x+1].setSide(thisPixelColor);
+                        break;
+                    case 3:
+                        if(y<NUMBER_OF_PIXELS-1)
+                            pixels[y+1][x].setSide(thisPixelColor);
+                        break;
+                }
+            }
+        }
+        
+        try{
+            Thread.sleep(20);
+        } catch(InterruptedException e){
+            System.err.println("Error sleeping Thread :: " + e);
+        }
+        
+        this.repaint();
+    }
+    
+}
